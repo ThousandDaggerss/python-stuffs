ede43e0f9c6ed272999ade9b4f0edb2d585cc9ee
Giovane Robino <giovanerobino@gmail.com>
2022-06-29 18:11:31 -0300

    implemented pow using class

---
+++ implemented_pow.py
@@ -0,0 +1,25 @@
+class solution:
+    def pow(self, x, n):
+        if x == 0 or x == 1 or n == 1:
+            return x
+        
+        if x == -1:
+            if n % 2 == 0:
+                return 1
+            else:
+                return -1
+        
+        if n == 0:
+            return 1
+        
+        if n < 0:
+            return 1 / self.pow(x, -n)
+        
+        valor = self.pow(x, n // 2)
+        if n % 2 == 0:
+            return valor * valor
+        return valor * valor * x
+
+x = int(input('Number: '))
+n = int(input('Pow: '))
+print(solution().pow(x, n))
