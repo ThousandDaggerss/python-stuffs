bf54b5d6d0592a45f04f21b959d73a11df2fa134
giovanerobino <giovanerobino@gmail.com>
2023-11-05 23:36:22 -0300

    final version v0.2

--- hangman_with_api.py
+++ hangman_with_api.py
@@ -2,15 +2,15 @@
 Hangman Project
 """
 import os
+import time
 import requests
 from dotenv import load_dotenv
+from utils.tools import clear_terminal
+
 load_dotenv()
 
 API_KEY = os.getenv("X_RAPIDAPI_KEY")
 HOST = os.getenv("X_RAPIDAPI_HOST")
-
-# print(API_KEY)
-# print(HOST)
 
 stages = ['''
   +---+
@@ -43,7 +43,8 @@ stages = ['''
  /|   |
       |
       |
-=========''', '''
+=========
+''', '''
   +---+
   |   |
   O   |
@@ -72,7 +73,11 @@ stages = ['''
 def hangman():
     """ Hangman game """
 
+    # Clear the terminal screen
+    clear_terminal()
+
     print("Welcome to Hangman!")
+    time.sleep(1)
 
     # Make a request to the API endpoint
     url = "https://wordsapiv1.p.rapidapi.com/words/"
@@ -85,8 +90,8 @@ def hangman():
 
     response = requests.get(url, headers=headers, params=querystring, timeout=10)
 
-    # print(response.json())  # Para ver a reposta e testar
-    # print(response.status_code)
+    # print(response.json())  # see the word just to test
+    # print(response.status_code)  # if it's 200, it worked
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -96,6 +101,14 @@ def hangman():
         attempts = 6
 
         while attempts > 0:
+            # Clear the terminal before displaying the new hangman stage and word state
+            clear_terminal()
+            # Display title every stage
+            print("""
+             _____ _____ _____ _____ _____ _____ _____ 
+            |  |  |  _  |   | |   __|     |  _  |   | |
+            |     |     | | | |  |  | | | |     | | | |
+            |__|__|__|__|_|___|_____|_|_|_|__|__|_|___|""")
             # Display the current hangman stage
             print(stages[6 - attempts])
 
@@ -124,8 +137,23 @@ def hangman():
             if all(letter in guessed_letters for letter in word):
                 print("Congratulations! You guessed the word.")
                 break
+
+        # Clear the terminal one more time before displaying the final result
+        clear_terminal()
+        # Display title
+        print("""
+             _____ _____ _____ _____ _____ _____ _____ 
+            |  |  |  _  |   | |   __|     |  _  |   | |
+            |     |     | | | |  |  | | | |     | | | |
+            |__|__|__|__|_|___|_____|_|_|_|__|__|_|___|""")
+        # Show the word when the game is over
+        print(stages[6 - attempts])  # Display the final hangman stage
         # show the word when game over
         print(f"The word was: {word}.\nGame over!")
+        # sleep for 3 seconds
+        time.sleep(4)
+        # Clear the terminal
+        clear_terminal()
     else:
         print("Failed to retrieve word from API.")
 
